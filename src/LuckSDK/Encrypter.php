3302eaf8a56cc6fa4ba4cf17ca097387b64689a6
tianyong90 <412039588@qq.com>
2015-12-20 21:14:56 +0800

    gitigonre and content.

--- src/LuckSDK/Encrypter.php
+++ src/LuckSDK/Encrypter.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * Encrypter.php
+ *
+ * Part of Tianyong90\LuckSDK.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author    tianyong90 <412039588@qq.com>
+ * @copyright 2015 tianyong90 <412039588@qq.com>
+ * @link      https://github.com/tianyong90
+ */
 
 namespace Tianyong90\LuckSDK;
 
